acc4df7e8abfeafc9291277a2b2b2a626d1e002a
Manraj2122 <98106001+Manraj2122@users.noreply.github.com>
2022-02-15 23:10:38 +0530

    Add files via upload

---
+++ Asg3_21103115_output.py
@@ -0,0 +1,70 @@
+#QUESTION1
+Enter any string: fleet
+{'f': 1, 'l': 1, 'e': 2, 't': 1}
+
+#QUESTION2
+Enter the day: 20
+Enter the month of the year: 12
+Enter the year: 1920
+21 / 12 / 1920
+
+#QUSETION3
+Enter elements of a list separated by space 2 30 32
+list:  2 30 32
+[(2, 4), (30, 900), (32, 1024)]
+
+#QUESTION4
+Enter Grade Point: 7
+Your Grade is 'B' and Good performance
+
+#QUESTION5
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+#QUESTION6
+Enter SID: 21103115
+Enter Name: Manraj Singh Chadha
+Do you want to enter another student entry(Y or N):Y
+Enter SID: 2003143
+Enter Name: John Scott
+Do you want to enter another student entry(Y or N):Y
+Enter SID: 2345678
+Enter Name: David Harley
+Do you want to enter another student entry(Y or N):N
+<a> {21103115: 'Manraj Singh Chadha', 2003143: 'John Scott', 2345678: 'David Harley'}
+<b> {2345678: 'David Harley', 2003143: 'John Scott', 21103115: 'Manraj Singh Chadha'}
+<c> {2003143: 'John Scott', 2345678: 'David Harley', 21103115: 'Manraj Singh Chadha'}
+Search Name with SID: 2003143
+<d> John Scott
+
+#QUESTION7
+Fibonacci sequence:
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+55
+89
+144
+233
+377
+610
+AVERAGE OF RESULTANT FIBONACCI SERIES IS 99.75
+
+#QUESTION8
+<a> {1, 3, 5, 6, 8}
+<b> {3, 6, 8, 9, 13, 17}
+<c> {1, 2, 4, 5}
+<d> {6, 7, 8, 9, 10}
+<e> {10, 7}
+<e> {10, 7}
